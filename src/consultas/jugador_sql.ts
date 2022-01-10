7d5e56835235497a95d87df227d89906c0b7fd8a
Luis Vicente Albarracin <luisalbarracin9727@gmail.com>
2022-01-10 16:59:48 -0500

    Modificacion Consulta sql

--- src/consultas/jugador_sql.ts
+++ src/consultas/jugador_sql.ts
@@ -1,6 +1,7 @@
 export const SQL_JUGADOR = {
-  'OBTENER_TODOS': 'SELECT j.cod_jugador, j.cod_usuario, u.nombres_usuario, \
-   u.apellidos_usuario, j.posicion_jugador, j.dorsal_jugador \
+  'OBTENER_TODOS': 'SELECT j.cod_jugador as codigojugador, \
+   j.cod_usuario as codigousuario, u.nombres_usuario as nombresusuario, u.documento_usuario as documentousuario, \
+   u.apellidos_usuario as apellidosusuario, j.posicion_jugador as posicionjugador, j.dorsal_jugador as dorsaljugador \
    FROM jugadores j \
    INNER JOIN usuarios u \
    ON j.cod_usuario = u.cod_usuario \
